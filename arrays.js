dddfa2a646d6ab4a2c4896441b1b97f46b4f176c
RS_ict <r.stefani87.informatica@gmail.com>
2024-03-03 00:49:20 +0100

    no message

--- arrays.js
+++ arrays.js
@@ -1,10 +1,21 @@
 /**
  * @prototype {array} 
+ * @param {array} this_array
+ * @param {function} filter
+ * @returns {number}
+ * @desc {en} Get the index of an element in an array
+ * @desc {it} Ottieni l'indice di un elemento in un array
+ * @desc {es} Obtener el indice de un elemento en un array
+ * @desc {fr} Trouver l'indice d'un élément dans un array
+ * @desc {de} Finde das Element in einem Array
+ * @desc {pt} Obter o índice de um elemento em um array
+ * @desc {zh} 获取数组中元素的索引
+ * @desc {ru} Найти индекс элемента в массиве
+ * @desc {ja} 配列の要素のインデックスを取得
+ * 
  */
-function indexOfFilter(this_array,filter){
+export function indexOfFilter(this_array,filter){
 	const element=this_array.filter(filter)[0]??null;
 	if(element) return this_array.indexOf(element);
 	return null;
 }
-
-module.exports = {indexOfFilter:indexOfFilter};
