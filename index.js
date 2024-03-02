fc2572ff045ffa1258c23b2b15b074a397802b3d
RS_ict <r.stefani87.informatica@gmail.com>
2024-03-02 11:12:59 +0100

    no message

--- index.js
+++ index.js
@@ -11,5 +11,6 @@
  * @desc {ru} Коллекция базовых инструментов aReS
  * @desc {ja} aReSフレームワークの基本ユーティリティ
  */
-
-
+import { initPrototypes } from '@ares/core/prototype';
+// init the prototypes by all functions in this module
+initPrototypes();
