bf6418a64062bcc1738b47647d1b1fdb15713f9a
RS_ict <r.stefani87.informatica@gmail.com>
2024-03-02 22:40:28 +0100

    no message

--- crypto.js
+++ crypto.js
@@ -1,11 +1,9 @@
 /**
  * @prototype {string}
  */
-function getMD5Hash(this_string){
+export function getMD5Hash(this_string){
 	const crypto = require('crypto');
 	const md5Hash = crypto.createHash('md5');
 	md5Hash.update(this_string);
 	return md5Value = md5Hash.digest('hex');
 }
-
-module.exports = {getMD5Hash:getMD5Hash};
