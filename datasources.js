8fcc11978c541ee1177d7ffce99fa45b17ffa3c5
RS_ict <r.stefani87.informatica@gmail.com>
2024-09-17 12:12:32 +0200

    fix: exported mysql driver dependency to @ares/datasource-mysql

--- datasources.js
+++ datasources.js
@@ -2,7 +2,7 @@
  * @author Roberto Stefani
  * @license MIT
  */
-import mysql from "mysql";
+
 import { v4 as uuidv4 } from "uuid";
 import { asyncConsole } from "./console.js";
 import { format } from "./dataDescriptors.js";
@@ -84,7 +84,7 @@ export class Datasource {
         this.sessions[req.session.id] = this.sessions[req.session.id] ?? {};
       }
       if (force || !this.sessions[req.session.id][mapper.connectionSetting]) {
-        const driverConstructor = drivers[connectionSetting.driver];
+        const driverConstructor = connectionSetting.driver;
         this.sessions[req.session.id][mapper.connectionSetting] =
           new driverConstructor(
             connectionSetting,
@@ -251,124 +251,6 @@ class DBConnection {
 
 class SQLDBConnection extends DBConnection {}
 
-class MariaDB extends SQLDBConnection {
-  async nativeConnect(callback) {
-    if (!MariaDB.pool) MariaDB.pool = mysql.createPool({...this, multipleStatements: true});
-    const dbConn = this;
-    this.connection = this.connection ?? null;
-    if (!this.connection) {
-      await MariaDB.pool.getConnection((err, conn) => {
-        if (err) {
-          console.error("Error getting connection:", err);
-          return;
-        }
-        conn.on("end", () => {
-          delete dbConn.datasource.sessions[this.sessionId];
-        });
-        this.connection = conn;
-        callback(err);
-      });
-    }
-  }
-
-  nativeDisconnect() {
-    this.connection.release((releaseError) => {
-      if (releaseError) {
-        console.error("Error releasing connection:", releaseError);
-        return;
-      }
-      delete this.datasource.sessions[this.sessionId];
-    });
-  }
-
-  startTransaction($name) {
-    if (!this.transaction) {
-      this.connection.beginTransaction((transactionError) => {
-        if (transactionError) {
-          throw new Error("Error on starting transaction: " + transactionError);
-        }
-        this.transaction = $name;
-      });
-    }
-  }
-
-  rollback($name) {
-    if (this.transaction === $name) {
-      this.connection.rollback();
-      this.transaction = null;
-    }
-  }
-
-  commit($name) {
-    if (this.transaction === $name) {
-      this.connection.commit((commitError) => {
-        if (commitError) {
-          throw new Error(
-            'Error on committing transaction "' + $name + '": ' + commitError
-          );
-        } else this.transaction = null;
-      });
-    }
-  }
-
-  async executeNativeQueryAsync(command, params, callback) {
-    const date = new Date();
-    const response = { executionTime: date.getTime(), executionDateTime: date };
-    const connectionHandler = this;
-    return new Promise(async (resolve, reject) => {
-      if (!connectionHandler.connection)
-        await connectionHandler.nativeConnect((err) => {
-          if (err) {
-            console.error("Errore di connessione:", err);
-            reject(err);
-            return;
-          }
-
-          connectionHandler.connection.query(
-            command,
-            params,
-            (error, results, fields) => {
-              response.executionTime =
-                new Date().getTime() - response.executionTime;
-              response.fields = fields;
-              response.results = results;
-              response.error = error;
-              if (!app.isProduction) {
-                response.query = command;
-                response.params = params;
-              }
-              callback(response);
-              if (error) {
-                reject(response);
-              } else {
-                resolve(response);
-              }
-            }
-          );
-        });
-    });
-  }
-  executeQuerySync(command, params, callback) {
-    const date = new Date();
-    const response = { executionTime: date.getTime(), executionDateTime: date };
-    const logName = "executeQuerySync_" + response.executionTime;
-    asyncConsole.log(logName, "Waiting for query results:");
-    let wait = true;
-    this.connection.query(command, params, (error, results, fields) => {
-      wait = false;
-      response.fields = fields;
-      response.results = results;
-      response.error = error;
-      response.executionTime = new Date().getTime() - response.executionTime;
-      callback(response);
-    });
-    while (wait) {
-      asyncConsole.log(logName, ".....");
-      setTimeout(() => {}, 100);
-    }
-    return response;
-  }
-}
 
 export class RESTConnection extends DBConnection {
   constructor(
@@ -421,16 +303,4 @@ export class RESTConnection extends DBConnection {
   }
 }
 
-const drivers = {
-  mariadb: MariaDB,
-  rest: RESTConnection,
-  // mysql: MYSQL,
-  // mssql: MSSQL,
-  // oracle: Oracle,
-  // postgres: Postgres,
-  // sqlite: SQLite,
-  // mongo: Mongo,
-  // redis: Redis,
-  // couchdb: CouchDB,
-  // neo4j: Neo4J
-};
+ 
