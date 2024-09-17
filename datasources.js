80116fbb2c435b3b23f0811c7e29d900e3bf4136
RS_ict <r.stefani87.informatica@gmail.com>
2024-09-17 14:50:46 +0200

    fix: export class SQLDBConnection

--- datasources.js
+++ datasources.js
@@ -249,7 +249,7 @@ class DBConnection {
   }
 }
 
-class SQLDBConnection extends DBConnection {}
+export class SQLDBConnection extends DBConnection {}
 
 
 export class RESTConnection extends DBConnection {
