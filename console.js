7ad08b31eee96850817efbfb59189ec6c654db57
RS_ict <r.stefani87.informatica@gmail.com>
2024-11-25 11:48:52 +0100

    feat: wrapping console

--- console.js
+++ console.js
@@ -3,6 +3,8 @@
  * @license MIT
  */
 
+
+
 /**
  * Async console: collect logs for showing them in a second later, all together, in the console when the process closes. Useful for showing logs of an asynchronous process.
  */
@@ -27,20 +29,60 @@ export const powerConsole = {
         console.log(`[${name}] \n\t${ Array.isArray(msg)?msg.join('\n\t\t'):msg }\n`);
     }
 };
+const oldConsole = {
+    log: console.log.bind(console),
+    error: console.error.bind(console),
+    warn: console.warn.bind(console),
+    info: console.info.bind(console),
+    debug: console.debug.bind(console),
+  };
 
-export function log(...messages){
-    console.log(...messages);
-}
+  // js standard
+ function getCallerInfo() {
+     const stack = new Error().stack;
+     if (stack) {
+       const stackLines = stack.split("\n");
+       if (stackLines.length > 3) {
+         const callerLine = stackLines[3].trim();
+         const match = callerLine.match(/at\s+(.+):(\d+):\d+/);
+         if (match) {
+           return `${match[1]}:${match[2]}`; 
+         }
+       }
+     }
+     return "unknown location";
+   }
 
-export function error(...messages){
-    console.error(...messages);
-}
-export function warn(...messages){
-    console.warn(...messages);
-}
-export function info(...messages){
-    console.info(...messages);
-}
-export function debug(...messages){
-    console.debug(...messages);
-}
+
+  
+  export function log(...messages) {
+    const callerInfo = getCallerInfo();
+    oldConsole.log( ...messages);
+  }
+  
+  export function error(...messages) {
+    const callerInfo = getCallerInfo();
+    oldConsole.error(`[${callerInfo}]`, ...messages);
+  }
+  
+  export function warn(...messages) {
+    const callerInfo = getCallerInfo();
+    oldConsole.warn(`[${callerInfo}]`, ...messages);
+  }
+  
+  export function info(...messages) {
+    const callerInfo = getCallerInfo();
+    oldConsole.info(  ...messages);
+  }
+  
+  export function debug(...messages) {
+    const callerInfo = '';
+    oldConsole.debug(`[${callerInfo}]`, ...messages);
+  }
+  
+  console.log = log;
+  console.error = error;
+  console.warn = warn;
+  console.info = info;
+  console.debug = debug;
+  
