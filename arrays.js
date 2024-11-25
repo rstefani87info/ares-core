3e03094459e6f630a49f73aecae6c61694298684
RS_ict <r.stefani87.informatica@gmail.com>
2024-11-26 00:00:36 +0100

    feat: object and array getSortedElements

--- arrays.js
+++ arrays.js
@@ -27,3 +27,25 @@ export function indexOfFilter(this_array, filter) {
 export function splitArray(this_seq, delimiter) {
   return lodash.split(Array.from(this_seq).join(delimiter), delimiter).map(subArr => subArr.split(delimiter));
 }
+
+
+
+/**
+ * Sorts elements from a settings object or array based on a provided mode function.
+ *
+ * @param {object|array} this_object_or_sequence - The input object or array to be sorted.
+ * @param {function} mode - A comparator function that determines the sort order.
+ * @return {array} An array of sorted elements, each element is an object with 'id' and 'value'.
+ * 
+ * @prototype {array}
+ * @prototype {Iterable}
+ * @prototype {object}
+ */
+export function getSortedElements(this_object_or_sequence, mode) {
+  const arr = [];
+  for (const [key, value] of (Array.isArray(this_object_or_sequence) || this_object_or_sequence instanceof Iterable ? this_object_or_sequence : Object.entries(this_object_or_sequence))) {
+    arr.push({  key, value });
+  }
+  arr.sort((a, b) => mode(a) - mode(b));
+  return arr;
+}
